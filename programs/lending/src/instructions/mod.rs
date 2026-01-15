bf3988bd34a8a33a918893ed7926955e87b6c330
MrBuluc <hakkicanbuluc@gmail.com>
2026-01-15 17:45:03 +0300

    initial commit

---
+++ programs/lending/src/instructions/mod.rs
@@ -0,0 +1,14 @@
+pub use admin::*;
+pub mod admin;
+
+pub use deposit::*;
+pub mod deposit;
+
+pub use withdraw::*;
+pub mod withdraw;
+
+pub use borrow::*;
+pub mod borrow;
+
+pub use repay::*;
+pub mod repay;
---
+++ programs/lending/src/lib.rs
@@ -0,0 +1,38 @@
+use anchor_lang::prelude::*;
+use instructions::*;
+
+mod constants;
+mod error;
+mod instructions;
+mod state;
+
+declare_id!("65JX6tyBwpGPxSnKCWyncAr8xFS2nDmA5mfmAAoqut8J");
+
+#[program]
+pub mod lending {
+    use super::*;
+
+    pub fn init_bank(
+        ctx: Context<InitBank>,
+        liquidation_threshold: u64,
+        max_ltv: u64,
+    ) -> Result<()> {
+        process_init_bank(ctx, liquidation_threshold, max_ltv)
+    }
+
+    pub fn init_user(ctx: Context<InitUser>, usdc_address: Pubkey) -> Result<()> {
+        process_init_user(ctx, usdc_address)
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        process_deposit(ctx, amount)
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        process_withdraw(ctx, amount)
+    }
+
+    pub fn borrow(ctx: Context<Borrow>, amount: u64) -> Result<()> {
+        process_borrow(ctx, amount)
+    }
+}
